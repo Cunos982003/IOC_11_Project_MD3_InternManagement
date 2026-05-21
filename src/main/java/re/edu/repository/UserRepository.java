67cb9cb90b8d562441b637c1cd677e3b3dbf7714
Cunos982003 <cuongjap208@gmail.com>
2026-05-21 12:49:59 +0700

    Thêm logic query cho UserRepo

--- src/main/java/re/edu/repository/UserRepository.java
+++ src/main/java/re/edu/repository/UserRepository.java
@@ -3,6 +3,8 @@ package re.edu.repository;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import re.edu.entity.User;
 import re.edu.util.Role;
 
@@ -13,4 +15,7 @@ public interface UserRepository extends JpaRepository<User, Long> {
     boolean existsByEmail(String email);
     boolean existsByUsername(String username);
     Page<User> findByRole(Role role, Pageable pageable);
+
+    @Query("SELECT u FROM User u WHERE u.id = :id")
+    Optional<User> findByIdWithDetails(@Param("id") Long id);
 }
